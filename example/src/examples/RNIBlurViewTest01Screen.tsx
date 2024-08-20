492750bb225bdc534a0b1bbaa435f0a841129908
Dominic Go <dominic@dominicgo.dev>
2024-08-20 21:10:27 +0800

    💫 Update: `RNIBlurViewTest01Screen`

--- example/src/examples/RNIBlurViewTest01Screen.tsx
+++ example/src/examples/RNIBlurViewTest01Screen.tsx
@@ -2,18 +2,62 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import { CardButton, Colors, ExampleItemCard, ObjectPropertyDisplay, UIBlurEffectStyleItems, type UIBlurEffectStyle } from 'react-native-ios-utilities';
+import { CardButton, Colors, ExampleItemCard, ObjectPropertyDisplay, UIBlurEffectStyleItems } from 'react-native-ios-utilities';
 import { RNIBlurView } from 'react-native-ios-visual-effect-view';
+import { AnimationConfigPresets } from '../constants/AnimationConfigPresets';
 
+
+var cache = {
+  blurEffectStyleCounter: 0,
+  animationConfigPresetCounter: 0,
+};
 
 export function RNIBlurViewTest01Screen() {
-  const [blurEffectStyleCounter, setBlurEffectStyleCounter] = React.useState(0); 
+
+  // MARK: blurEffectStyle
+  // ---------------------
+
+  const [
+    blurEffectStyleCounter, 
+    setBlurEffectStyleCounter
+  ] = React.useState(cache.blurEffectStyleCounter);
 
   const blurEffectStyleIndex = 
     blurEffectStyleCounter % UIBlurEffectStyleItems.length;
 
   const blurEffectStyleCurrent = UIBlurEffectStyleItems[blurEffectStyleIndex]!;
 
+  // MARK: animationConfigPreset
+  // ---------------------------
+
+  const [
+    animationConfigPresetCounter, 
+    setAnimationConfigPresetCounter
+  ] = React.useState(cache.animationConfigPresetCounter);
+
+  const animationConfigPresetIndex = 
+    animationConfigPresetCounter % AnimationConfigPresets.length;
+
+  const animationConfigPresetCurrent = AnimationConfigPresets[animationConfigPresetIndex]!;
+
+  const hasAnimationConfig = animationConfigPresetCurrent != null;
+
+  // MARK: animationDelay
+  // --------------------
+
+  const [animationDelay, setAnimationDelay] = React.useState<number | undefined>(
+    hasAnimationConfig ? 0.5 : undefined
+  );
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setAnimationDelay(undefined);
+    }, 1000);
+  }, []);
+
+  // MARK: render
+  // ------------
+  
   return (
     <View style={styles.container}>
       <Text style={styles.label}>
@@ -25,6 +69,8 @@ export function RNIBlurViewTest01Screen() {
           mode: 'standard',
           blurEffectStyle: blurEffectStyleCurrent,
         }}
+        animationConfig={animationConfigPresetCurrent}
+        animationDelay={animationDelay}
       />
       <SafeAreaView style={styles.debugOverlayContainer}>
         <ExampleItemCard
@@ -32,19 +78,43 @@ export function RNIBlurViewTest01Screen() {
           style={styles.debugCard}
         >
           <ObjectPropertyDisplay
-            recursiveStyle={styles.debugDisplay}
+            recursiveStyle={styles.debugDisplayInner}
             object={{
-              blurEffectStyleCounter,
+              animationDelay,
               blurEffectStyleCurrent: {
                 [blurEffectStyleIndex]: blurEffectStyleCurrent,
               },
+              animationConfigPreset: animationConfigPresetCurrent,
             }}
           />
           <CardButton
             title={'Next Blur Effect'}
-            subtitle={'Apply next blur effect'}
+            subtitle={
+                `Counter: ${blurEffectStyleCounter},`
+              + ` preset: ${blurEffectStyleIndex + 1} of ${UIBlurEffectStyleItems.length}`
+            }
             onPress={() => {
-              setBlurEffectStyleCounter((prevValue) => prevValue + 1);
+              setBlurEffectStyleCounter((prevValue) => {
+                const newValue = prevValue + 1;
+                cache.blurEffectStyleCounter = newValue;
+
+                return newValue;
+              });
+            }}
+          />
+          <CardButton
+            title={'Next Animation Preset'}
+            subtitle={
+                `Counter: ${animationConfigPresetCounter},`
+              + ` preset: ${animationConfigPresetIndex + 1} of ${AnimationConfigPresets.length}`
+            }
+            onPress={() => {
+              setAnimationConfigPresetCounter((prevValue) => {
+                const newValue = prevValue + 1;
+                cache.animationConfigPresetCounter = newValue;
+
+                return newValue;
+              });
             }}
           />
         </ExampleItemCard>
@@ -82,7 +152,7 @@ const styles = StyleSheet.create({
   debugCard: {
     backgroundColor: Colors.PURPLE[100],
   },
-  debugDisplay: {
+  debugDisplayInner: {
     backgroundColor: `${Colors.PURPLE[200]}99`,
   },
 });
