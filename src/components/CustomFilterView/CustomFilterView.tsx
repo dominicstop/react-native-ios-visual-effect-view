670baf6456b8b40b73bde02663a14e9bd576ee3b
Dominic Go <dominic@dominicgo.dev>
2024-12-18 21:13:52 +0800

    🐞 Fix: `CustomFilterView`

---
+++ src/components/CustomFilterView/CustomFilterView.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { View } from 'react-native';
+
+import { RNIVisualEffectCustomFilterView } from '../../native_components/RNIVisualEffectCustomFilterView';
+import { LIB_ENV } from '../../constants/LibEnv';
+
+import type { CustomFilterViewProps } from './CustomFilterViewTypes';
+
+
+export function CustomFilterView(props: CustomFilterViewProps){
+  const { currentFilters, children, ...viewProps } = props;
+
+  if(!LIB_ENV.isVisualEffectViewSupported) {
+    return (
+    <View {...viewProps} >
+      {children}
+    </View>
+    );
+  };
+
+  return (
+    <RNIVisualEffectCustomFilterView
+      {...viewProps}
+      currentFilters={currentFilters}
+    >
+      {children}
+    </RNIVisualEffectCustomFilterView>
+  );
+};
