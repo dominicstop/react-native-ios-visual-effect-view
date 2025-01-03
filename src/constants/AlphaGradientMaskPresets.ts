5a6d1404d1123db354b1018c057617e81419401d
Dominic Go <dominic@dominicgo.dev>
2025-01-03 15:13:52 +0800

    💫 Update: Add More Gradient Presets to `AlphaGradientMaskPresets`

--- src/constants/AlphaGradientMaskPresets.ts
+++ src/constants/AlphaGradientMaskPresets.ts
@@ -205,6 +205,146 @@ const Presets = {
     ],
     size: WINDOW_SIZE,
   },
+
+  'opaqueLeftAndRightClearCenter': {
+    type: 'axial',
+    startPointPreset: 'centerLeft',
+    endPointPreset: 'centerRight',
+    colors: [
+      createColorAlpha(1),
+      createColorAlpha(0),
+      createColorAlpha(1),
+    ],
+    size: WINDOW_SIZE,
+  },
+  'opaqueLeftAndRightClearCenterOffsetPlus30': {
+    type: 'axial',
+    startPointPreset: 'centerLeft',
+    endPointPreset: 'centerRight',
+    colors: [
+      createColorAlpha(1),
+      createColorAlpha(0.8),
+      createColorAlpha(0),
+      createColorAlpha(0.8),
+      createColorAlpha(1),
+    ],
+    size: WINDOW_SIZE,
+  },
+  'opaqueLeftAndRightClearCenterOffsetPlus50': {
+    type: 'axial',
+    startPointPreset: 'centerLeft',
+    endPointPreset: 'centerRight',
+    colors: [
+      createColorAlpha(1),
+      createColorAlpha(1),
+      createColorAlpha(0),
+      createColorAlpha(1),
+      createColorAlpha(1),
+    ],
+    size: WINDOW_SIZE,
+  },
+  'opaqueLeftAndRightClearCenterOffsetMinus30': {
+    type: 'axial',
+    startPointPreset: 'centerLeft',
+    endPointPreset: 'centerRight',
+    colors: [
+      createColorAlpha(1),
+      createColorAlpha(0.3),
+      createColorAlpha(0),
+      createColorAlpha(0.3),
+      createColorAlpha(1),
+    ],
+    size: WINDOW_SIZE,
+  },
+  'opaqueLeftAndRightClearCenterOffsetMinus50': {
+    type: 'axial',
+    startPointPreset: 'centerLeft',
+    endPointPreset: 'centerRight',
+    colors: [
+      createColorAlpha(1),
+      createColorAlpha(0),
+      createColorAlpha(0),
+      createColorAlpha(0),
+      createColorAlpha(1),
+    ],
+    size: WINDOW_SIZE,
+  },
+
+  'opaqueCenterClearLeftAndRight': {
+    type: 'axial',
+    startPointPreset: 'centerLeft',
+    endPointPreset: 'centerRight',
+    colors: [
+      createColorAlpha(0),
+      createColorAlpha(1),
+      createColorAlpha(0),
+    ],
+    size: WINDOW_SIZE,
+  },
+  'opaqueCenterClearLeftAndRightOffsetPlus30': {
+    type: 'axial',
+    startPointPreset: 'centerLeft',
+    endPointPreset: 'centerRight',
+    colors: [
+      createColorAlpha(0),
+      createColorAlpha(0.8),
+      createColorAlpha(1),
+      createColorAlpha(0.8),
+      createColorAlpha(0),
+    ],
+    size: WINDOW_SIZE,
+  },
+  'opaqueCenterClearLeftAndRightOffsetPlus50': {
+    type: 'axial',
+    startPointPreset: 'centerLeft',
+    endPointPreset: 'centerRight',
+    colors: [
+      createColorAlpha(1),
+      createColorAlpha(1),
+      createColorAlpha(0),
+      createColorAlpha(1),
+      createColorAlpha(1),
+    ],
+    size: WINDOW_SIZE,
+  },
+
+  'opaqueTopAndBottomClearCenter': {
+    type: 'axial',
+    startPointPreset: 'topCenter',
+    endPointPreset: 'bottomCenter',
+    colors: [
+      createColorAlpha(1),
+      createColorAlpha(0),
+      createColorAlpha(1),
+    ],
+    size: WINDOW_SIZE,
+  },
+  'opaqueTopAndBottomClearCenterOffsetPlus30': {
+    type: 'axial',
+    startPointPreset: 'topCenter',
+    endPointPreset: 'bottomCenter',
+    colors: [
+      createColorAlpha(1),
+      createColorAlpha(0.8),
+      createColorAlpha(0),
+      createColorAlpha(0.8),
+      createColorAlpha(1),
+    ],
+    size: WINDOW_SIZE,
+  },
+  'opaqueTopAndBottomClearCenterOffsetPlus50': {
+    type: 'axial',
+    startPointPreset: 'topCenter',
+    endPointPreset: 'bottomCenter',
+    colors: [
+      createColorAlpha(1),
+      createColorAlpha(1),
+      createColorAlpha(0),
+      createColorAlpha(1),
+      createColorAlpha(1),
+    ],
+    size: WINDOW_SIZE,
+  },
 };
 
 type PresetKeys = keyof typeof Presets;
